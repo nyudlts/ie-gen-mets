e48c23e3326e42533a886ed9402b30f28db952c0
Joseph Pawletko <jgpawletko@gmail.com>
2014-03-25 19:10:37 -0400

    snapshot

--- test/ie-gen-mets_test.rb
+++ test/ie-gen-mets_test.rb
@@ -3,60 +3,66 @@ require 'open3'
 
 class TestIeGenMets < Test::Unit::TestCase
 
+  COMMAND  = 'ruby bin/ie-gen-mets.rb'
   VALID_IE = 'test/ies/valid'
   EMPTY_IE = 'test/ies/empty-dir'
   CANONICAL_XML  = 'test/canonical/valid_mets.xml'
+  MPTR_1  = 'nyu_aco000177_mets.xml#s-ie-00000001'
+  MPTR_2  = 'nyu_aco000178_mets.xml#s-ie-00000001'
+  MISSING = 'MISSING'
+  MPTR_4  = 'nyu_aco000180_mets.xml#s-ie-00000001'
 
   def test_exit_status_with_valid_text
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' #{VALID_IE}")
+    o, e, s = Open3.capture3("#{COMMAND} '6efa1021-7453-4150-8d4a-705899530d8e' #{VALID_IE} #{MPTR_1} #{MPTR_2} #{MISSING} #{MPTR_4}")
     assert(s == 0, "incorrect exit status")
     assert_match(/<mets xmlns/, o, "no mets output detected")
   end
 
+=begin
   def test_with_incorrect_argument_count
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb")
+    o, e, s = Open3.capture3("#{COMMAND}")
     assert(s != 0, "incorrect argument count")
     assert(o == '')
     assert_match(/incorrect number of arguments/, e, 'unexpected error message')
   end
 
   def test_with_invalid_dir
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' invalid-dir-path")
+    o, e, s = Open3.capture3("#{COMMAND} '6efa1021-7453-4150-8d4a-705899530d8e' 'SOURCE_ENTITY:TEXT' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' invalid-dir-path")
     assert(s != 0, "incorrect exit status")
     assert(o == '')
     assert_match(/directory does not exist/, e, 'unexpected error message')
   end
 
   def test_invalid_se_type
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'INVALID' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' #{VALID_IE}")
+    o, e, s = Open3.capture3("#{COMMAND} '6efa1021-7453-4150-8d4a-705899530d8e' 'INVALID' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' #{VALID_IE}")
     assert(s != 0)
     assert(o == '')
     assert_match(/incorrect se type/, e, 'unexpected error message')
   end
 
   def test_invalid_binding_orientation
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'INVALID' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' #{VALID_IE}")
+    o, e, s = Open3.capture3("#{COMMAND} '6efa1021-7453-4150-8d4a-705899530d8e' 'SOURCE_ENTITY:TEXT' 'INVALID' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' #{VALID_IE}")
     assert(s != 0)
     assert(o == '')
     assert_match(/incorrect binding orientation/, e, 'unexpected error message')
   end
 
   def test_invalid_scan_order
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'INVALID' 'RIGHT_TO_LEFT' #{VALID_IE}")
+    o, e, s = Open3.capture3("#{COMMAND} '6efa1021-7453-4150-8d4a-705899530d8e' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'INVALID' 'RIGHT_TO_LEFT' #{VALID_IE}")
     assert(s != 0)
     assert(o == '')
     assert_match(/incorrect scan order/, e, 'unexpected error message')
   end
 
   def test_invalid_read_order
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'RIGHT_TO_LEFT' 'INVALID' #{VALID_IE}")
+    o, e, s = Open3.capture3("#{COMMAND} '6efa1021-7453-4150-8d4a-705899530d8e' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'RIGHT_TO_LEFT' 'INVALID' #{VALID_IE}")
     assert(s != 0)
     assert(o == '')
     assert_match(/incorrect read order/, e, 'unexpected error message')
   end
 
   def test_missing_md_files
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'RIGHT_TO_LEFT' 'LEFT_TO_RIGHT' #{EMPTY_IE}")
+    o, e, s = Open3.capture3("#{COMMAND} '6efa1021-7453-4150-8d4a-705899530d8e' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'RIGHT_TO_LEFT' 'LEFT_TO_RIGHT' #{EMPTY_IE}")
     assert(s != 0)
     assert(o == '')
     assert_match(/missing or too many files ending in _mods\.xml/, e)
@@ -67,28 +73,28 @@ class TestIeGenMets < Test::Unit::TestCase
   end
 
   def test_mismatched_master_dmaker_file_count
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'RIGHT_TO_LEFT' 'LEFT_TO_RIGHT' #{BAD_M_D_COUNT_IE}")
+    o, e, s = Open3.capture3("#{COMMAND} '6efa1021-7453-4150-8d4a-705899530d8e' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'RIGHT_TO_LEFT' 'LEFT_TO_RIGHT' #{BAD_M_D_COUNT_IE}")
     assert(s != 0)
     assert(o == '')
     assert_match(/mismatch in master \/ dmaker file count/, e)
   end
 
   def test_mismatched_master_dmaker_file_prefixes
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'RIGHT_TO_LEFT' 'LEFT_TO_RIGHT' #{BAD_M_D_COUNT_IE}")
+    o, e, s = Open3.capture3("#{COMMAND} '6efa1021-7453-4150-8d4a-705899530d8e' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'RIGHT_TO_LEFT' 'LEFT_TO_RIGHT' #{BAD_M_D_COUNT_IE}")
     assert(s != 0)
     assert(o == '')
     assert_match(/mismatch in master \/ dmaker file count/, e)
   end
 
   def test_mismatched_master_dmaker_file_prefixes
-    o, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'RIGHT_TO_LEFT' 'LEFT_TO_RIGHT' #{BAD_M_D_PREFIX_IE}")
+    o, e, s = Open3.capture3("#{COMMAND} '6efa1021-7453-4150-8d4a-705899530d8e' 'SOURCE_ENTITY:TEXT' 'HORIZONTAL' 'RIGHT_TO_LEFT' 'LEFT_TO_RIGHT' #{BAD_M_D_PREFIX_IE}")
     assert(s != 0)
     assert(o == '')
     assert_match(/prefix mismatch:/, e)
   end
 
   def test_output_with_valid_text
-    new_xml, e, s = Open3.capture3("ruby bin/text-gen-mets.rb 'nyu_aco000003' 'SOURCE_ENTITY:TEXT' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' #{VALID_IE}")
+    new_xml, e, s = Open3.capture3("#{COMMAND} '6efa1021-7453-4150-8d4a-705899530d8e' 'SOURCE_ENTITY:TEXT' 'VERTICAL' 'LEFT_TO_RIGHT' 'RIGHT_TO_LEFT' #{VALID_IE}")
     assert(s == 0)
     old_xml, e, s = Open3.capture3("cat #{CANONICAL_XML}")
     new_xml_a = new_xml.split("\n")
@@ -107,5 +113,5 @@ class TestIeGenMets < Test::Unit::TestCase
       assert(new == old, "xml mismatch: #{new} #{old}")
     end
   end
-
+=end
 end
